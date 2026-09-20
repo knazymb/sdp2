90c015e8967524c7e6fb73ebd28f2a8aaeea7296
knazymb <102862467+knazymb@users.noreply.github.com>
2026-09-20 16:29:44 +0500

    delivery

--- src/application/DeliveryApplication.java
+++ src/application/DeliveryApplication.java
@@ -1,4 +1,30 @@
 package application;
 
+import abstractfactory.Button;
+import abstractfactory.Checkbox;
+import abstractfactory.GUIFactory;
+import factorymethod.AbstractLogistics;
+
 public class DeliveryApplication {
+
+    private final GUIFactory guiFactory;
+    private final AbstractLogistics logistics;
+
+    public DeliveryApplication(GUIFactory guiFactory, AbstractLogistics logistics) {
+        this.guiFactory = guiFactory;
+        this.logistics = logistics;
+    }
+
+    public void run() {
+        Button button = guiFactory.createButton();
+        Checkbox checkbox = guiFactory.createCheckbox();
+
+        button.paint();
+        checkbox.paint();
+
+        logistics.planDelivery(
+                "laboratory equipment",
+                "Aktau warehouse"
+        );
+    }
 }
